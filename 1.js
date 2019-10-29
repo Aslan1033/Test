92ebddd4ad86a4788ae4a30ec9e5a42bf0081118
Aslan1033 <aslan.isayev.1997@gmail.com>
2019-10-29 12:04:55 +0400

    test v1

---
+++ 1.js
@@ -0,0 +1,23 @@
+
+let img = 1;
+Show();
+
+setInterval(function() {Change("next"); }, 3000);
+
+
+function Show() {
+    document.getElementById("field").style.backgroundImage = `url("img/${img}.jpg")`;
+}
+
+function Change(btn) {
+    switch (btn) {
+        case "prev":
+            img = img < 2 ? 7 : img - 1;
+            Show();
+            break;
+        case "next":
+            img = img > 6 ? 1 : img + 1;
+            Show();
+            break;
+    }
+}
